3189c0107e1b414977a4440e37c0d549b0e7247d
ruaefd2 <Daniil.EFREMOV@raiffeisen.ru>
2024-10-31 13:50:07 +0300

    fix

--- java-spring-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -1,23 +1,19 @@
 package exercise;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
-
+import exercise.model.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
-import exercise.model.Post;
+import java.net.URI;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
