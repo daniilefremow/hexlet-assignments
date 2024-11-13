95c37494b389302f69896cc465cf09b51792a5e8
@hexlet/cli <support@hexlet.io>
2024-11-13 10:15:03 +0300

    submit java-spring-ru/jpa-repository

---
+++ java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package exercise.repository;
+
+import exercise.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // BEGIN
+    List<Product> findByPriceBetweenOrderByPrice(Integer startPrice, Integer endPrice);
+    // END
+}
